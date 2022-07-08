e5ddd282e0864097ad731ed466cf59e40f4e8c3f
19Ted <antoniosandroni@gmail.com>
2022-07-08 17:35:36 +0200

    de

--- prova.py
+++ prova.py
@@ -1,11 +1,23 @@
-class MyClass:
-    variabile="cane"
-    def MyFunction(self):
-        return self.variabile
-     
-y=MyClass()
-x=MyClass()
+class CarClass:
+    name=""
+    kind=""
+    color=""
+    price=""
+    def description(self):
+        descrizione="L'auto di nome %s è %s di colore %s e costa %s" % (self.name,self.kind,self.color,self.price)
+        return descrizione
 
-x.variabile="gatto"
+macchina1=CarClass()
+macchina1.name="Ladro"
+macchina1.kind="un trattore"
+macchina1.color="arcobaleno"
+macchina1.price="2 miliardi di euro"
 
-print(x.MyFunction())
+macchina2=CarClass()
+macchina2.name="Shrek"
+macchina2.kind="una spider"
+macchina2.color="viola"
+macchina2.price="50 cent"
+
+print(macchina1.description())
+print(macchina2.description())
