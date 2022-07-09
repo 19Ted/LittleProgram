17a927f911c7ab982ac95ee59ed85bbfab4ee31b
19Ted <antoniosandroni@gmail.com>
2022-07-09 09:50:59 +0200

    fo

---
+++ supporto.py
@@ -0,0 +1,7 @@
+class Cani:
+    razza=""
+    età:int 
+    nome=""
+    def descrive(self):
+        descrizione="Il cane %s è di razza %s e ha %d anni" % (self.nome,self.razza,self.età)
+        return descrizione
