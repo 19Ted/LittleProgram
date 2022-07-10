7b7624c264d18c324a7bb2206554bc1d5cf7f2e8
19Ted <antoniosandroni@gmail.com>
2022-07-10 18:33:56 +0200

    err

--- supporto.py
+++
@@ -1,7 +0,0 @@
-class Cani:
-    razza=""
-    età:int 
-    nome=""
-    def descrive(self):
-        descrizione="Il cane %s è di razza %s e ha %d anni" % (self.nome,self.razza,self.età)
-        return descrizione
